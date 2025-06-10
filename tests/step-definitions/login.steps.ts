b8df0a36f8c77fcb71c0113782c5cc873dd52e07
sindz <nhanthanhdang2003@gmail.com>
2025-06-10 16:08:59 +0700

    Update Jenkinsfile with agent and tools configuratioann and login.step.ts

--- tests/step-definitions/login.steps.ts
+++ tests/step-definitions/login.steps.ts
@@ -5,25 +5,25 @@ import { page } from '../hooks/hooks';
 
 let loginPage: LoginPage;
 
-Given('I am on the login page', async () => {
+Given('I am on the login page', { timeout: 15000 }, async () => {
   loginPage = new LoginPage(page);
   await loginPage.navigate();
 });
 
-When('I enter username {string} and password {string}', async (username: string, password: string) => {
+When('I enter username {string} and password {string}', { timeout: 15000 }, async (username: string, password: string) => {
   await loginPage.login(username, password);
 });
 
-When('I click the login button', async () => {
+When('I click the login button', { timeout: 15000 }, async () => {
   // Đã tích hợp trong hàm login, không cần bước riêng
 });
 
-Then('I should see the success message {string}', async (expectedMessage: string) => {
+Then('I should see the success message {string}', { timeout: 15000 }, async (expectedMessage: string) => {
   const actualMessage = await loginPage.getSuccessMessage();
   expect(actualMessage).toContain(expectedMessage);
 });
 
-Then('I should see the error message {string}', async (expectedMessage: string) => {
+Then('I should see the error message {string}', { timeout: 15000 }, async (expectedMessage: string) => {
   const actualMessage = await loginPage.getErrorMessage();
   expect(actualMessage).toContain(expectedMessage);
 });
