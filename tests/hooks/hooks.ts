adf6da6b40e2bd834f870046d6e627bd1d56cebf
sindz <nhanthanhdang2003@gmail.com>
2025-06-16 11:33:26 +0700

    Update

--- tests/hooks/hooks.ts
+++ tests/hooks/hooks.ts
@@ -18,15 +18,14 @@ BeforeAll(async () => {
         
         headless: true, 
        });
-  page.setDefaultTimeout(60000); // 60 giây cho các action (ví dụ: click, fill, waitForSelector)
-  page.setDefaultNavigationTimeout(90000); // 90 giây cho navigation (ví dụ: page.goto)
+
 });
 
 Before(async () => {
   page = await browser.newPage();
   // Thiết lập timeout mặc định trực tiếp cho page
-  // page.setDefaultTimeout(60000); // 60 giây cho các action (ví dụ: click, fill, waitForSelector)
-  // page.setDefaultNavigationTimeout(90000); // 90 giây cho navigation (ví dụ: page.goto)
+  page.setDefaultTimeout(60000); // 60 giây cho các action (ví dụ: click, fill, waitForSelector)
+  page.setDefaultNavigationTimeout(90000); // 90 giây cho navigation (ví dụ: page.goto)
 });
 
 After(async () => {
